81cba020ea3c26812d13beba8b972edbbbe2f845
Harsh <harshuniverse000@gmail.com>
2023-01-19 17:11:50 +0530

    Navbar and home

--- components/layout.js
+++ components/layout.js
@@ -14,31 +14,29 @@ const Layout = ({ children }) => {
 export const Navbar = () => {
   return (
     <>
-      {/* <div className={styles.container}> */}
-        <div className={styles.header}>
-          <div className={styles.content}>
-            <div className={styles.logoBlock}>
-              <Image
-                src="/LogoBlock.png"
-                alt="Epvi-Logo"
-                width="95"
-                height="32"
-              />
-            </div>
-            <div className={styles.links}>
-              <Link href="">Our Product</Link>
-              <Link href="">Our App</Link>
-              <Link href="">FAQ</Link>
-              <Link href="">Blogs</Link>
-              <Link href="">About</Link>
-            </div>
-            <div className={styles.actions}>
-              <button id={styles.btn1}>Contact Us</button>
-              <button id={styles.btn2}>Get Smi-Fi</button>
-            </div>
+      <div className={styles.navContainer}>
+        <div className={styles.navContent}>
+          <div className={styles.logoBlock}>
+            <Image
+              src="/LogoBlock.png"
+              alt="Epvi-Logo"
+              width="95"
+              height="32"
+            />
+          </div>
+          <div className={styles.navLinks}>
+            <Link href="">Our Product</Link>
+            <Link href="">Our App</Link>
+            <Link href="">FAQ</Link>
+            <Link href="">Blogs</Link>
+            <Link href="">About</Link>
+          </div>
+          <div className={styles.navActions}>
+            <button id={styles.btn1}>Contact Us</button>
+            <button id={styles.btn2}>Get Smi-Fi</button>
           </div>
         </div>
-      {/* </div> */}
+      </div>
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,63 @@
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <div className={styles.main}>
+        <GetSmifi />
+      </div>
+    </>
+  );
+}
+// getSmifi
+function GetSmifi() {
+  return (
+    <>
+      <div className={styles.getSmifi_container}>
+        <div className={styles.getSmifi_content}>
+          <div className={styles.getSmifi_contentText}>
+            <div className={styles.getSmifi_heading_overline}>
+              <div className={styles.getSmifi_energyAssistant}>
+                YOUR HOME ENERGY ASSISTANT
+              </div>
+              <div className={styles.getSmifi_smarterHomes}>
+                <p>
+                  Smi-Fi makes your Home <span>Smarter.</span>
+                </p>
+              </div>
+            </div>
+            <div className={styles.supportText}>
+              Get the control of your entire home on your phone
+            </div>
+          </div>
+          <div className={styles.getSmifi_action}>
+            <button id={styles.getSmifi_btn}>
+              Get Smi-Fi
+              <FontAwesomeIcon icon={faArrowRight} />
+            </button>
+            <div className={styles.getSmifi_rating}>
+                <Image
+                  width="120"
+                  height="24"
+                  src="/Rating.png"
+                  alt="rating-img"
+                />
+              <div className={styles.getSmifi_ratingText}>From 100+ reviews</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.picture4_1}>
+          <Image
+            src="/Picture4 _1.png"
+            width="550"
+            height="506"
+            alt="Smi-fi and App"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
